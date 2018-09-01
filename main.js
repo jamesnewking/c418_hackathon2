c8a48009c311d2eab79ec6be776379f13036e92f
Daniel S <danielksstone@gmail.com>
2018-08-31 21:52:52 -0700

    modified some html and css properties for visual purposes

--- main.js
+++ main.js
@@ -3,21 +3,9 @@ $(document).ready(loadDocument);
 var winningCity;
 var muteVol = 1;
 
+
 function loadDocument(){
-    $('.material-icons').on('click',function(){
-        if($('.material-icons').text()==='volume_up'){
-            $('.material-icons').text('volume_off');
-            muteVol = 0;
-            airplaneSound.volume = 0.0;
-        } else { $('.material-icons').text('volume_up');
-            muteVol = 1;
-            airplaneSound.volume = 1.0;
-            }
-    });
-    var airplaneSound = new Audio('audio/airplane.mp3');
-
-    airplaneSound.play();
-
+    clPreloader();
 };
 
 function insertWeatherInfo(cityInfo, hintInfo) {
@@ -162,3 +150,34 @@ function handleButtonClick() {
     }
 }
 
+$WIN = $(window);
+
+// Add the User Agent to the <html>
+// will be used for IE10 detection (Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0))
+var doc = document.documentElement;
+doc.setAttribute('data-useragent', navigator.userAgent);
+
+
+   /* Preloader
+    * -------------------------------------------------- */
+   var clPreloader = function() {
+        
+    $("html").addClass('cl-preload');
+
+    $WIN.on('load', function() {
+
+        //force page scroll position to top at page refresh
+        // $('html, body').animate({ scrollTop: 0 }, 'normal');
+
+        // will first fade out the loading animation 
+        $("#loader").fadeOut("slow", function() {
+            // will fade out the whole DIV that covers the website.
+            $("#preloader").delay(300).fadeOut("slow");
+        }); 
+        
+        // for hero content animations 
+        $("html").removeClass('cl-preload');
+        $("html").addClass('cl-loaded');
+    
+    });
+};
